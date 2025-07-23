6b8c71caf4b005fead0e13123df87beb6e01db9e
manjaripandey <manjari22pandey@gmail.com>
2025-07-23 12:44:03 +0000

    initial commit

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,14 @@
+import { createContext,useState } from "react";
+export const AppContext=createContext()
+const AppContextProvider=(props)=>{
+    const[user,Setuser] = useState(null);
+    const value ={
+        user,Setuser
+    }
+    return (
+        <AppContext.Provider value={value}> 
+        {props.children}
+        </AppContext.Provider>
+    )
+}
+export default AppContextProvider
