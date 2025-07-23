bf6d773c1651527794e8a248fbbebd35e2ba845f
manjaripandey <manjari22pandey@gmail.com>
2025-07-23 12:59:21 +0000

    test2

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
   return (
     <div className='flex items-center justify-between py-4'>
       <Link to='/'>
-        <img src={assets.logo} alt="Logo" className='w-28 sm:w-32 lg:w-40' />
+        <img src={assets.logo} alt="LOGO" className='w-28 sm:w-32 lg:w-40' />
       </Link>
 
       <div>
