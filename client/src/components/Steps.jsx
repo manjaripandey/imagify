2f92d848f835abbe15c7aead44f89ea680b49861
manjaripandey <manjari22pandey@gmail.com>
2025-07-26 14:26:35 +0000

    test5

--- client/src/components/Steps.jsx
+++ client/src/components/Steps.jsx
@@ -9,7 +9,7 @@ const Steps = () => {
       <div className='space-y-4 w-fu;; max-w-3xl text-sm'>
         {stepsData.map((item,index)=>(
             <div key={index}
-            className='flex items-center gap-4 p-5 px-8 bg-white/20 shadow-md border cursor-pointer hover:scale-[1.02] transition-all duration-300'>
+            className='flex items-center gap-4 p-5 px-8 bg-white/20 shadow-md border cursor-pointer hover:scale-[1.02] transition-all duration-300 rounded-lg'>
 
                 <img width={40} src={item.icon} alt="" />
                 <div>
